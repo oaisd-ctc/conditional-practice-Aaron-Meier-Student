b83693f3c425f84328c9752ba48f5037895cf9bb
Aaron-Meier-Student <aamea72@student.oaisd.org>
2024-10-21 13:29:31 +0000

    Created all required functions and made them work.

--- Program.cs
+++ Program.cs
@@ -1,26 +1,89 @@
+using System;
+using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        FahrenheitToCelsius(68); // Test FahrenheitToCelsius Function with a random value
+        CheckForPositiveNegativeZero(0);
+        CheckForPositiveNegativeZero(1);
+        CheckForPositiveNegativeZero(-1);
+        Console.WriteLine();
 
-        // Test your functions by calling them below:
-        // CheckForPositiveNegativeZero(-1); // This should output: "Your number is negative"
-        // CheckForPositiveNegativeZero(0); // This should output: "Your number is zero"
-        // CheckForPositiveNegativeZero(1); // This should output: "Your number is positive"
-        // ...
-        // ...
-        // DisplayDayOfWeek(0); // This should output: "Thursday"
+        FindMinimum(24,12,6);
+        FindMinimum(24,3,6);
+        FindMinimum(2,3,6);
+        Console.WriteLine();
 
+        FindMaximum(24,12,6);
+        FindMaximum(24,3,65);
+        FindMaximum(2,34,6);
+        Console.WriteLine();
+
+        IsDivisibleBy5(5);
+        IsDivisibleBy5(23);
+        IsDivisibleBy5(30);
+        Console.WriteLine();
+
+        CheckEvenOrOdd(5);
+        CheckEvenOrOdd(22);
+        CheckEvenOrOdd(30);
+        Console.WriteLine();
+
+        CheckVowelOrConsonant('e');
+        CheckVowelOrConsonant('h');
+        CheckVowelOrConsonant('A');
+        Console.WriteLine();
+
+        DisplayDayOfWeek(3);
+        DisplayDayOfWeek(7);
+        DisplayDayOfWeek(5);
     }
 
-    public static void FahrenheitToCelsius(double fahrenheit)
+    public static void CheckForPositiveNegativeZero(double number)
     {
-        double celsiusDegree = (fahrenheit - 32) * 5/9; 
-        Console.WriteLine($"{fahrenheit} degrees Fahrenheit is {celsiusDegree} degrees Celsius.");
+        Console.WriteLine($"Your Number is {(number > 0 ? "Positive" : number < 0 ? "Negative" : "Zero")}.");
     }
 
-    // Define your functions below:
+    public static void FindMinimum(int x, int y, int z)
+    {
+        int min = x;
+        min = min > y ? y : min;
+        min = min > z ? z : min;
+        Console.WriteLine($"The minimum value is {min}.");
+    }
 
+    public static void FindMaximum(int x, int y, int z)
+    {
+        int max = x;
+        max = max < y ? y : max;
+        max = max < z ? z : max;
+        Console.WriteLine($"The maximum value is {max}.");
+    }
+
+    public static void IsDivisibleBy5(int x)
+    {
+        Console.WriteLine($"{x} is {(x%5 != 0 ? "not " : "")}divisible by 5.");
+    }
+
+    public static void CheckEvenOrOdd(int x)
+    {
+        Console.WriteLine($"{x} is an {(x%2 == 0 ? "Even" : "Odd")} number.");
+    }
+
+    public static void CheckVowelOrConsonant(char x)
+    {
+        char[] vowels = {'a', 'e', 'i', 'o', 'u', 'y'};
+        Console.WriteLine($"{x} is a {(vowels.Contains(Char.ToLower(x)) ? "Vowel" : "Consonant")}.");
+    }
+
+    public static void DisplayDayOfWeek(int day)
+    {
+        string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+        Console.WriteLine(days[day - 1]);
+    }
 }
